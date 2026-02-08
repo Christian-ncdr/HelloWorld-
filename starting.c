2d18b1cfd5b714a6c59faf0131a2872f536d5663
Christian-Olano <olanochristianjohnpaul@gmail.com>
2026-02-08 23:03:33 +0800

    using pointer in C

--- starting.c
+++ starting.c
@@ -1,14 +1,56 @@
 #include <stdio.h>
 
+int getSum(int *arr, int n);
+float getAverage(int *arr, int n);
+int getMax(int *arr, int n);
+
 int main() {
-    printf("Hello, World!\n");
-    printf("this is my day 1 of learning C programming language.\n");
+   int n = 0;
+   int arr[100];
 
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
 
+    for(int i = 0 ; i < n ; i++){
+        printf("Enter the element %d: ", i + 1);
+        scanf("%d", arr + i);
+    }
+
+    int sum = getSum(arr, n);
+    float average = getAverage(arr, n);
+    int max = getMax(arr, n);
+
+    printf("sum %d\n", sum);
+    printf("average %.2f\n", average);
+    printf("max %d\n", max);
 
 
 
     return 0;
 
 
+}
+
+int getSum(int *arr, int n){
+    int sum = 0;
+    for (int i = 0; i < n ; i++){
+        sum += *(arr + i);
+    }
+
+    return sum;
+}
+
+float getAverage(int *arr, int n){
+    return (float)getSum(arr, n)/ n;
+}
+
+int getMax(int *arr, int n){
+    int max = *arr;
+    for(int i = 0; i < n ; i++) {
+        if  (*(arr + i) > max){
+             max = *(arr + i);
+        }
+    }
+
+    return max;
 }
